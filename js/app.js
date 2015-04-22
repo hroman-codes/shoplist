51d62e3168cffe2298e4bbeafc237e8b30c978b8
iamromanh <romanh99@gmail.com>
2015-04-22 16:05:05 -0400

    updated js file checkbox

--- js/app.js
+++ js/app.js
@@ -4,15 +4,18 @@ $(document).ready(function() {
 
 		var userInput = $(".shoppingitem").val();
 
-		$(".list-area-active").append("<li>" + "<input type='checkbox'>" + userInput + "</li>")	
+		$(".list-area-active").append("<<li></li>>" + "<input type='checkbox'>" + userInput + "</li>")	
 	});
+$(document).on("click", "li", function() {
+        $(this).remove();
+    });
 });
 
 
-$(".clearbutton").on("click", function(event) {
-		event.preventDefault();
+// $(".clearbutton").on("click", function(event) {
+// 		event.preventDefault();
 
-		var userInput = $(".clearbutton").val();
+// 		var userInput = $(".clearbutton").val();
 
-		$(".list-area-active", "li").hide();
-});
+// 		$(".list-area-active", "li").hide();
+// });
